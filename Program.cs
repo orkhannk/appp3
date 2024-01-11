f51e1b9b68045bb5cceca33d3bd59ca2d046b3b3
orkhannk <orkhannk@code.edu.az>
2024-01-11 17:29:47 +0400

    files added

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fullname daxil edin :");
+            string FullName1 = Console.ReadLine();
+            Console.WriteLine("Point daxil edin :");
+            string point1Str = Console.ReadLine();
+            int point1 = Convert.ToInt32(point1Str);
+            Console.WriteLine("grup nomresini daxil edin :");
+            
+            Student student = new Student(FullName1, point1)
+            {
+                
+                GroupNo = Console.ReadLine(),
+                
+            };
+
+            student.ShowStudents();
+
+
+
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Student
+
+    {
+        public Student (string FullName,int Point)
+        {
+            this.Point = Point;
+            this.FullName = FullName;
+        }
+
+        public Student (string FullName,int Point,string GroupNo) : this(FullName, Point)
+        {
+            this.GroupNo = GroupNo;
+        }
+
+        public Student (string Fullname,int Point,string GroupNo,DateTime BirthDay): this(Fullname, Point, GroupNo)
+        {
+            this.BirthDay = BirthDay;
+        }
+
+
+
+
+        public string FullName;
+        public string GroupNo;
+        public DateTime BirthDay;
+        public int  Point;
+
+
+        public void ShowStudents()
+        {
+            Console.WriteLine($"{this.FullName} - {this.GroupNo}-{this.BirthDay.ToString("dd.MM.yyyy")} - {this.Point}");
+
+
+        }
+
+
+        
+    }
+}
